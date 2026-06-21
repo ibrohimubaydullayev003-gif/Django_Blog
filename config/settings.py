dbbb535314bdda826a64b4fa2ff46fcd4493bc40
ibrohim <ibrohimubaydullayev003@gmail.com>
2026-06-21 18:08:16 +0500

    _2

--- config/settings.py
+++ config/settings.py
@@ -153,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ibrohimubaydullayev003@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtxr hshe qtri hroh'  
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''  
